b9b9f50b5c1f9d5076ca8d7c612af27fb576375f
Jacob Daniels <s3933490@student.rmit.edu.au>
2025-12-23 23:06:40 +0800

    Upgraded filesize

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (USE_SUPABASE) {
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB limit
+    fileSize: 20 * 1024 * 1024, // 5MB limit
   },
   fileFilter: (req, file, cb) => {
     // Accept images only
